7d9e0ebf2113f5b419e04a249e33e42127b3e82d
huan_kong <2564076459@qq.com>
2023-07-02 00:08:05 +0800

    修复评论

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,49 +1,47 @@
-import { plumeTheme } from "@vuepress-plume/vuepress-theme-plume";
-import notes from "./notes";
-import navbar from "./navbar";
+import { plumeTheme } from '@vuepress-plume/vuepress-theme-plume'
+import notes from './notes'
+import navbar from './navbar'
 
 export default plumeTheme({
-  logo: "https://img.huankong.top/i/2022/11/29/6385ef8c7a675.ico",
+  logo: 'https://img.huankong.top/i/2022/11/29/6385ef8c7a675.ico',
   appearance: true,
-  hostname: "https://blog.huankong.top/",
+  hostname: 'https://blog.huankong.top/',
   avatar: {
-    name: "huan_kong",
-    url: "https://img.huankong.top/i/2022/11/29/6385f13b5bb93.png",
-    description: "世间的美好总是不期而遇",
+    name: 'huan_kong',
+    url: 'https://img.huankong.top/i/2022/11/29/6385f13b5bb93.png',
+    description: '世间的美好总是不期而遇'
   },
-  social: [{ icon: "github", link: "https://github.com/huankong233" }],
+  social: [{ icon: 'github', link: 'https://github.com/huankong233' }],
   navbar,
   notes,
   footer: {
-    copyright: "Copyright © 2020-present huan_kong",
+    copyright: 'Copyright © 2020-present huan_kong'
   },
   themePlugins: {
     shiki: {
-      theme: "one-dark-pro",
+      theme: 'one-dark-pro'
     },
     markdownEnhance: {
-      katex: true,
+      katex: true
     },
     search: {
       locales: {
-        "/": {
-          placeholder: "Search",
+        '/': {
+          placeholder: 'Search'
         },
-        "/zh/": {
-          placeholder: "搜索",
-        },
-      },
+        '/zh/': {
+          placeholder: '搜索'
+        }
+      }
     },
     comment: {
-      provider: "Giscus",
+      provider: 'Giscus',
       comment: true,
-      repo: "pengzhanbo/pengzhanbo.cn",
-      repoId: "MDEwOlJlcG9zaXRvcnkxNDgwMzY4MDc=",
-      category: "Blog-Comment",
-      categoryId: "DIC_kwDOCNLcx84CUulO",
-      mapping: "pathname",
-      reactionsEnabled: true,
-      inputPosition: "top",
-    },
-  },
-});
+      repo: 'huankong233/blog-page',
+      repoId: 'R_kgDOI84cvQ',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOI84cvc4CUo2w',
+      mapping: 'title'
+    }
+  }
+})
