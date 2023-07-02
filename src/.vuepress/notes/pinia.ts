b2d860083ef681d5ca1b307c058867feeaa0a687
huan_kong <2564076459@qq.com>
2023-07-02 09:16:34 +0800

    更新

--- src/.vuepress/notes/pinia.ts
+++ src/.vuepress/notes/pinia.ts
@@ -4,5 +4,12 @@ export default definePlumeNotesItemConfig({
   text: 'pinia',
   dir: 'pinia',
   link: '/pinia',
-  sidebar: ['']
+  sidebar: [
+    '',
+    {
+      text: '学习',
+      dir: 'learn',
+      items: ['使用', '解构访问', '数据修改', 'action', 'getters']
+    }
+  ]
 })
