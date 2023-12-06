1e849cc6a95501fcd97f723756a8c1a90078377d
huan_kong <2564076459@qq.com>
2023-12-06 09:20:33 +0800

    [+] 增加集合/字典

---
+++ src/note/algorithm/2.数据结构/字典.ts
@@ -0,0 +1,36 @@
+class Map {
+  #values: { [key: string]: any } = {}
+
+  has(key: string) {
+    return this.#values.hasOwnProperty(key)
+  }
+
+  set(key: string, value: any) {
+    this.#values[key] = value
+  }
+
+  remove(key: string) {
+    if (!this.has(key)) return false
+    delete this.#values[key]
+  }
+
+  get(key: string) {
+    return this.has(key) ? this.#values[key] : undefined
+  }
+
+  keys() {
+    return Object.keys(this.#values)
+  }
+
+  values() {
+    return Object.values(this.#values)
+  }
+
+  size() {
+    return this.keys().length
+  }
+
+  clear() {
+    this.#values = {}
+  }
+}
---
+++ src/note/algorithm/2.数据结构/集合.ts
@@ -0,0 +1,91 @@
+class Set {
+  #values: { [key: string]: any } = {}
+
+  add(value: any) {
+    if (this.has(value)) return false
+    return (this.#values[value] = value)
+  }
+
+  remove(value: any) {
+    if (!this.has(value)) return false
+    return delete this.#values[value]
+  }
+
+  has(value: any) {
+    return this.#values[value] !== undefined
+  }
+
+  clear() {
+    return (this.#values = {})
+  }
+
+  size() {
+    return Object.keys(this.#values).length
+  }
+
+  values() {
+    return Object.values(this.#values)
+  }
+
+  toString() {
+    return Object.values(this.#values).join(' , ')
+  }
+
+  union(otherSet: Set) {
+    const unionSet = new Set()
+
+    for (const value of this.values()) {
+      unionSet.add(value)
+    }
+
+    for (const value of otherSet.values()) {
+      unionSet.add(value)
+    }
+
+    return unionSet
+  }
+
+  intersection(otherSet: Set) {
+    const intersectionSet = new Set()
+
+    for (const value of otherSet.values()) {
+      if (this.has(value)) intersectionSet.add(value)
+    }
+
+    return intersectionSet
+  }
+
+  difference(otherSet: Set) {
+    const differenceSet = new Set()
+
+    for (const value of otherSet.values()) {
+      if (!this.has(value)) differenceSet.add(value)
+    }
+
+    return differenceSet
+  }
+
+  subSet(otherSet: Set) {
+    // 方案2
+    // let count = 0
+    // for (const value of otherSet.values()) {
+    //   if (this.has(value)) count++
+    // }
+    // return count === otherSet.size()
+
+    // 方案1
+    for (const value of otherSet.values()) {
+      if (!this.has(value)) return false
+    }
+
+    return true
+  }
+}
+
+const set = new Set()
+set.add(123)
+const set2 = new Set()
+set2.add(12322)
+set2.add(12323)
+set2.add(123)
+console.log(set2.subSet(set))
